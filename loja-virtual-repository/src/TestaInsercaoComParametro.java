b531fd7fa2ff36e552cd8f18717fb8b7e6263484
Letícia Fernandes <leticia.fernandes@tmax.com.br>
2021-02-25 15:39:19 -0300

    Inserção com prepare statement

---
+++ loja-virtual-repository/src/TestaInsercaoComParametro.java
@@ -0,0 +1,33 @@
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class TestaInsercaoComParametro {
+
+	public static void main(String[] args) throws SQLException {
+		String nome = "MOUSE'";
+		String descricao = "MOUSE SEM FIO";
+		
+		ConnectionFactory connectionFactory = new ConnectionFactory();
+		Connection connection = connectionFactory.recuperarConexao();
+		
+		PreparedStatement stm = connection.prepareStatement("INSERT INTO produto (nome, descricao) VALUES (?, ?)", 
+				Statement.RETURN_GENERATED_KEYS);
+		
+		stm.setString(1, nome);
+		stm.setString(2, descricao);
+		
+		stm.execute();
+		
+		ResultSet rst =  stm.getGeneratedKeys();
+		
+		while (rst.next()) {
+			Integer id = rst.getInt(1);
+			System.out.println("O id criado foi: " + id);
+		}
+
+	}
+
+}
