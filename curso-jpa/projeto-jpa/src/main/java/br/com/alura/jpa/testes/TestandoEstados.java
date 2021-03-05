c25ca010d38a9bfc2addca753880b71de4454ad9
lesfernandes <lemayara16@gmail.com>
2021-03-05 17:59:22 -0300

    managed, removed e transient

---
+++ curso-jpa/projeto-jpa/src/main/java/br/com/alura/jpa/testes/TestandoEstados.java
@@ -0,0 +1,35 @@
+package br.com.alura.jpa.testes;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import br.com.alura.jpa.modelo.Conta;
+
+public class TestandoEstados {
+
+	public static void main(String[] args) {
+		// Transient - existe na memória, possui informações
+		// não possui id, mas pode ser managed
+		Conta conta = new Conta();
+		conta.setTitular("Almiro");
+		conta.setAgencia(664662);
+		conta.setNumero(66266);
+		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("contas");
+		EntityManager em = emf.createEntityManager();
+		
+		em.getTransaction().begin();
+		
+		// Transient -> Managed
+		// sincronização automática com o banco
+		//em.persist(conta);
+		
+		// Managed -> Removed
+		// remover do banco de dados (delete)
+		em.remove(conta);
+		
+		em.getTransaction().commit();
+	}
+
+}
